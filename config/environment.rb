7a8678cb01e20b0bf44366723f30b9ac93d5cf1f
Muhammed zeeshan <muhammedzeeshan488@gmail.com>
2023-02-08 11:10:05 +0530

    chages

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-ActionMailer::Base.delivery_method = :smtp # be sure to choose SMTP delivery
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 465,
-  :domain               => "gmail.com",
-  :user_name            => "muhammedzeeshan488@gmail.com",
-  :password             => "123456",
-  :authentication       => "login",
-  :enable_starttls_auto => true
-}
+
+
+
